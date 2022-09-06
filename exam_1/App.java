833fd921839141aaa95a9ef8742177afc53c6069
Jonnpatrickhaclad <111546613+Jonnpatrickhaclad@users.noreply.github.com>
2022-09-06 10:13:03 +0800

    Create App.java

---
+++ exam_1/App.java
@@ -0,0 +1,17 @@
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+    Student Me = new Student();
+    Me.firstname = "Jonn Patrick";
+    Me.middleInital = 'B' ;
+    Me.surname = "Haclad";
+    Me.dateOfBirth = 6/13/2004;
+    Me.studentNumber = 2022104243;
+    Me.studentEmailAddress = "hacladjb@students.national-u.edu.ph";
+    Me.IAmAwsome = true;
+    Me.sayMystudentNumber();
+    Me.studentEmailAddress();
+    Me.IAmAwsome();
+  }
+} 
